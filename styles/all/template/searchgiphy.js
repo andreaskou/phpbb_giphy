d4af74ee10d7cbf984961db9a2b05689fa566a2a
andreaskou <akourtidis@gmail.com>
2021-04-03 14:45:50 +0200

    fixing the search box and result area

--- styles/all/template/searchgiphy.js
+++ styles/all/template/searchgiphy.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
 
     let apiKey = 'Oa1dCJJTFFfduUE9A4V5rv4qgJZWiD21';
     let route = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`;
+    let query_url;
+    let offset;
+    let results_count;
 
     // Getting the current value of textarea
     var currentText = $(this).val();
@@ -27,6 +30,9 @@ $(document).ready(function(){
         if (e.keyCode == '13')
         {
             e.preventDefault();
+        }
+        else {
+
         }
 
         // Make a new timeout set to go off in 1000ms (1 second)
@@ -54,35 +60,38 @@ $(document).ready(function(){
         }, 1000);
     });
 
+    // $(".giphy_btn_wrapper")
+    // .filter(function(){
+    //     return $(this).find("div:visible").length == 0;
+    // }).hide();
+
     function renderImg(images)
     {
-        var wrapperDivWidth = document.getElementById("giphy_wrapper").offsetWidth;
-        console.log(wrapperDivWidth);
+        // var wrapperDivWidth = document.getElementById("giphy_wrapper").offsetWidth;
+        // console.log(wrapperDivWidth);
         var giphyResultsDiv = document.getElementById("giphy_results");
-        if (giphyResultsDiv.hasChildNodes())
-        {
-            alert('I have child(s)');
-        }
-        else {
-            alert('I am not a parrent yet!');
-        }
+        // if (giphyResultsDiv.hasChildNodes())
+        // {
+        //     alert('I have child(s)');
+        // }
         giphyResultsDiv.innerHTML = "";
-        // document.getElementById("giphy_results").innerHTML ="";
+        document.getElementById("giphy_results").innerHTML ="";
 
         for (var i = 0; i < images.data.length; i++)
         {
             if (images.meta.status == 200)
             {   var img = document.createElement("img");
-                img.setAttribute("src", images.data[i].images.preview_gif.url);
+                img.setAttribute("src", images.data[i].images.fixed_height_small.url);
                 img.setAttribute("alt", images.data[i].title);
                 img.setAttribute("id", images.data[i].id);
-                img.setAttribute("class", "giphy_image");
+                img.setAttribute("class", "item");
                 img.onclick = function (){
                     let attach = fetchImages(this.id, true);
                 }
                 document.getElementById("giphy_results").appendChild(img);
             }
         }
+
     }
 
     function fetchImages(query, id = false)
@@ -92,6 +101,7 @@ $(document).ready(function(){
             query = `https://api.giphy.com/v1/gifs/${query}?api_key=${apiKey}`;
         }
 
+        query_url = query;
         fetch(query)
         .then( response => response.json() )
         .then( content => {
@@ -99,8 +109,12 @@ $(document).ready(function(){
                 insert_text('[img]' + content.data.images.original.url + '[/img]');
             }
             else {
+                offset = content.pagination.offset;
+                results_count = content.pagination.total_count;
+                loadPreviousButton();
                 renderImg(content);
-                console.log(content);
+                // console.log(content);
+                loadNextButton();
             }
         })
         .catch(err=>{
@@ -112,6 +126,58 @@ $(document).ready(function(){
     {
         alert(img_id);
         fetchImages(img_id, true);
+    }
+
+    function loadPreviousButton()
+    {
+        var buttonPrevious = document.getElementById("giphy_previous_btn");
+        if (offset === 0)
+        {
+            // console.log(buttonPrevious.style);
+            if (buttonPrevious.style.display === "block")
+            {
+                buttonPrevious.style.display="none";
+                // buttonPrevious.style.visibility='hidden';
+            }
+        }
+        else
+        {
+            if (buttonPrevious.style.display === "none")
+            {
+                buttonPrevious.style.display="block";
+            }
+        }
+    }
+
+    function loadNextButton()
+    {
+        var buttonNext = document.getElementById("giphy_next_btn");
+        if (results_count >= 10)
+        {
+            if (buttonNext.style.display === "none")
+            {
+                buttonNext.style.display = "block";
+            }
+        }
+        else
+        {
+            if (buttonNext.style.display === "block")
+            {
+                buttonNext.style.display="none";
+            }
+        }
+
+        // console.log('This is the url ' + query_url);
+        console.log('This is the offset ' + offset);
+        console.log('This is the results_count ' + results_count);
+        // if (offset = 0)
+        // {
+        //     var next_button = document.createElement('BTN');
+        //     next_button.setAttribute('type', 'button');
+        //     var button_text = document.createTextNode("Load Next");
+        //     next_button.appendChild(button_text);
+        //     document.getElementById("giphy_results").appendChild(next_button);
+        // }
     }
 
     // let hover = document.getElementsByClassName("giphy_image");
