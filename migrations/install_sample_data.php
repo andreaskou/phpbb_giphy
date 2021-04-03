737caf2e4aa20ed410434351ca92ed022299e96a
andreaskou <akourtidis@gmail.com>
2021-04-03 14:44:39 +0200

    Setting up user permissions

--- migrations/install_sample_data.php
+++ migrations/install_sample_data.php
@@ -35,23 +35,21 @@ class install_sample_data extends \phpbb\db\migration\migration
 	{
 		return array(
 			// Add new permissions
-			array('permission.add', array('a_new_andreask_giphyforphpbb')), // New admin permission
-			array('permission.add', array('m_new_andreask_giphyforphpbb')), // New moderator permission
+			// array('permission.add', array('a_new_andreask_giphyforphpbb')), // New admin permission
+			// array('permission.add', array('m_new_andreask_giphyforphpbb')), // New moderator permission
 			array('permission.add', array('u_new_andreask_giphyforphpbb')), // New user permission
 
-			// array('permission.add', array('a_copy', true, 'a_existing')), // New admin permission a_copy, copies permission settings from a_existing
-
 			// Set our new permissions
-			array('permission.permission_set', array('ROLE_ADMIN_FULL', 'a_new_andreask_giphyforphpbb')), // Give ROLE_ADMIN_FULL a_new_andreask_giphyforphpbb permission
-			array('permission.permission_set', array('ROLE_MOD_FULL', 'm_new_andreask_giphyforphpbb')), // Give ROLE_MOD_FULL m_new_andreask_giphyforphpbb permission
-			array('permission.permission_set', array('ROLE_USER_FULL', 'u_new_andreask_giphyforphpbb')), // Give ROLE_USER_FULL u_new_andreask_giphyforphpbb permission
+			// array('permission.permission_set', array('ROLE_ADMIN_FULL', 'a_new_andreask_giphyforphpbb')), // Give ROLE_ADMIN_FULL a_new_andreask_giphyforphpbb permission
+			// array('permission.permission_set', array('ROLE_MOD_FULL', 'm_new_andreask_giphyforphpbb')), // Give ROLE_MOD_FULL m_new_andreask_giphyforphpbb permission
+			// array('permission.permission_set', array('ROLE_USER_FULL', 'u_new_andreask_giphyforphpbb')), // Give ROLE_USER_FULL u_new_andreask_giphyforphpbb permission
 			array('permission.permission_set', array('ROLE_USER_STANDARD', 'u_new_andreask_giphyforphpbb')), // Give ROLE_USER_STANDARD u_new_andreask_giphyforphpbb permission
 			array('permission.permission_set', array('REGISTERED', 'u_new_andreask_giphyforphpbb', 'group')), // Give REGISTERED group u_new_andreask_giphyforphpbb permission
-			array('permission.permission_set', array('REGISTERED_COPPA', 'u_new_andreask_giphyforphpbb', 'group', false)), // Set u_new_andreask_giphyforphpbb to never for REGISTERED_COPPA
+			// array('permission.permission_set', array('REGISTERED_COPPA', 'u_new_andreask_giphyforphpbb', 'group', false)), // Set u_new_andreask_giphyforphpbb to never for REGISTERED_COPPA
 
 			// Add new permission roles
-			array('permission.role_add', array('giphyforphpbb admin role', 'a_', 'a new role for admins')), // New role "giphyforphpbb admin role"
-			array('permission.role_add', array('giphyforphpbb moderator role', 'm_', 'a new role for moderators')), // New role "giphyforphpbb moderator role"
+			// array('permission.role_add', array('giphyforphpbb admin role', 'a_', 'a new role for admins')), // New role "giphyforphpbb admin role"
+			// array('permission.role_add', array('giphyforphpbb moderator role', 'm_', 'a new role for moderators')), // New role "giphyforphpbb moderator role"
 			array('permission.role_add', array('giphyforphpbb user role', 'u_', 'a new role for users')), // New role "giphyforphpbb user role"
 		);
 	}
